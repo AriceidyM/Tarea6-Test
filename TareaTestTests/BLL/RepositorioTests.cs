29644d58b6f0176cf025d24038fe6549d9c3b94b
Rosy <Rosy@DESKTOP-HI3GI4E>
2019-06-22 14:56:47 -0400

    Comenzando a hacer el test

---
+++ TareaTestTests/BLL/RepositorioTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TareaTest.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaTest.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
